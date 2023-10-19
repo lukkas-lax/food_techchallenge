0db1eebc3d7fb9745a8e92554fbf0c1db5086107
lukit <lukkitaalves6@gmail.com>
2023-10-19 20:05:06 -0300

    Commit Inicial

---
+++ food_techchallenge/src/main/java/com/fiap/food_techchallenge/FoodTechchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.food_techchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTechchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTechchallengeApplication.class, args);
+	}
+
+}
---
+++ food_techchallenge/src/test/java/com/fiap/food_techchallenge/FoodTechchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.food_techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTechchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
