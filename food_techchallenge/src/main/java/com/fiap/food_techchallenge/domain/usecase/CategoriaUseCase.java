c5b6158f75c0ea725c5c09c26224151e4653c2f3
lukit <lukkitaalves6@gmail.com>
2023-10-20 21:16:58 -0300

    Feat: retira import

--- food_techchallenge/src/main/java/com/fiap/food_techchallenge/domain/usecase/CategoriaUseCase.java
+++ food_techchallenge/src/main/java/com/fiap/food_techchallenge/domain/usecase/CategoriaUseCase.java
@@ -2,7 +2,6 @@ package com.fiap.food_techchallenge.domain.usecase;
 
 import com.fiap.food_techchallenge.domain.Categoria;
 import com.fiap.food_techchallenge.domain.ports.inbound.CategoriaUseCasePort;
-import com.fiap.food_techchallenge.domain.ports.inbound.ProdutoUseCasePort;
 import com.fiap.food_techchallenge.domain.ports.outbound.CategoriaAdapterPort;
 
 public class CategoriaUseCase implements CategoriaUseCasePort {
