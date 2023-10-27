d73610b3090686245da59ae4cdd48782a64a7bc5
lukit <lukkitaalves6@gmail.com>
2023-10-27 20:01:52 -0300

    Feat: Alteras script SQL

--- food_techchallenge/database_creation.sql
+++ food_techchallenge/database_creation.sql
@@ -1,9 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Schema food_techchallenge
 -- -----------------------------------------------------
@@ -42,8 +36,8 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `food_techchallenge`.`pedidos` (
   `id` BIGINT NOT NULL,
-  `datapedido` DATE NULL DEFAULT NULL,
-  `order_status` TINYINT NULL DEFAULT NULL,
+  `datapedido` DATETIME(6) NULL DEFAULT NULL,
+  `order_status` VARCHAR(255) NULL DEFAULT NULL,
   `total` FLOAT NULL DEFAULT NULL,
   `user_id` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
@@ -76,12 +70,11 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `food_techchallenge`.`itenspedido` (
   `id` BIGINT NOT NULL,
-  `quantidade` INT NULL DEFAULT NULL,
   `pedido_id` BIGINT NULL DEFAULT NULL,
   `produto_id` BIGINT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `UK_hxtdpu0hdvjj6qek53v4cb6x8` (`produto_id` ASC) VISIBLE,
   INDEX `FK1sfuslkhoh2oa3ps0fom0muxm` (`pedido_id` ASC) VISIBLE,
+  INDEX `FK9yhqgrgxeymyxf8xpli8hry43` (`produto_id` ASC) VISIBLE,
   CONSTRAINT `FK1sfuslkhoh2oa3ps0fom0muxm`
     FOREIGN KEY (`pedido_id`)
     REFERENCES `food_techchallenge`.`pedidos` (`id`),
@@ -96,6 +89,8 @@ COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
 
 INSERT INTO `food_techchallenge`.`categorias` (`id`, `descricao`) VALUES ('1', 'Lanche');
 INSERT INTO `food_techchallenge`.`categorias` (`id`, `descricao`) VALUES ('2', 'Acompanhamento');
