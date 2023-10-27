2baffa8fe3533536a95bf471d8f9ab3ca34eafdd
lukit <lukkitaalves6@gmail.com>
2023-10-27 18:25:31 -0300

    Feat: Retira imports não utilizados

--- food_techchallenge/src/main/java/com/fiap/food_techchallenge/application/adapter/outbound/entity/ItensPedidoEntity.java
+++ food_techchallenge/src/main/java/com/fiap/food_techchallenge/application/adapter/outbound/entity/ItensPedidoEntity.java
@@ -5,10 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
-
 @Entity
 @Getter
 @Setter
